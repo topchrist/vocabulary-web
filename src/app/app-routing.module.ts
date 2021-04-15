f147027c7e213b5ef1c7d4f6ab1fe0611f309f77
Chris <christophedjonteu@gmail.com>
2021-04-15 08:16:00 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LevelEditComponent } from './level-edit/level-edit.component';
+import { LevelListComponent } from './level-list/level-list.component';
+import { WordEditComponent } from './word-edit/word-edit.component';
+import { WordListComponent } from './word-list/word-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', component: LevelListComponent },
+  { path: 'levels', component: LevelListComponent },
+  { path: 'level-add', component: LevelEditComponent },
+  { path: 'level-edit/:id', component: LevelEditComponent },
+  { path: 'words', component: WordListComponent },
+  { path: 'word-add', component: WordEditComponent },
+  { path: 'word-edit/:id', component: WordEditComponent },
+  // { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LevelListComponent } from './level-list/level-list.component';
+import { LevelEditComponent } from './level-edit/level-edit.component';
+import { WordEditComponent } from './word-edit/word-edit.component';
+import { WordListComponent } from './word-list/word-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LevelAddComponent } from './level-add/level-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LevelListComponent,
+    LevelEditComponent,
+    WordEditComponent,
+    WordListComponent,
+    LevelAddComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
